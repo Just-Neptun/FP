f7850c80a8cb97442a162e2a229b408394a45dcd
Flaming-H-Phoenix <249494353+Flaming-H-Phoenix@users.noreply.github.com>
2026-06-30 21:28:51 +0200

    clean CNN code and add hyperparameter optimized version

--- S10 Machine Learning/ml_fp/Part03/CNN_implementation.py
+++ S10 Machine Learning/ml_fp/Part03/CNN_implementation.py
@@ -1,13 +1,10 @@
 from torch import nn
 
-
-
 class Neural_Network(nn.Module):
     def __init__(self):
         super().__init__()
-        # comments show expected/decided shape between steps in sequence
-        # ----- input -----
-        # batch *  3 * 64 * 64
+        # each MaxPool2d(2, 2) halves both spatial dimensions (starting at 64 x 64)
+        # each Conv2d matches in and out channels to previous and next (input has 3)
         self.conv_layers = nn.Sequential(
             nn.Conv2d(
                 in_channels=3,
@@ -17,12 +14,11 @@ class Neural_Network(nn.Module):
                 padding=1
             ),
             nn.ReLU(),
-            # batch * 32 * 64 * 64
             nn.MaxPool2d(
                 kernel_size=2,
                 stride=2
             ),
-            # batch * 32 * 32 * 32
+
             nn.Conv2d(
                 in_channels=32,
                 out_channels=64,
@@ -31,21 +27,73 @@ class Neural_Network(nn.Module):
                 padding=1
             ),
             nn.ReLU(),
-            # batch * 64 * 32 * 32
             nn.MaxPool2d(
                 kernel_size=2,
                 stride=2
             )
-            # batch * 64 * 16 * 16
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            # batch * (64 * 16 * 16)
             nn.Linear(64 * 16 * 16, 128),
-            # batch * 128
             nn.ReLU(),
+            nn.Dropout(0.5),
+
             nn.Linear(128, 4)
-            # batch * 4
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = self.classifier(x)
+        return x
+
+
+class Neural_Network_Optimized(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(
+                in_channels=3,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            ),
+
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=16,
+                kernel_size=3,
+                stride=1,
+                padding=1
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(
+                kernel_size=2,
+                stride=2
+            )
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear((16 * 16 * 16), 128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128, 4)
         )
 
     def forward(self, x):
