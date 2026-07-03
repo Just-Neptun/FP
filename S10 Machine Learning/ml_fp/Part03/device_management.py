1c4699093d7410e5fd1eebf1b87aa34bd6c33ac9
Flaming-H-Phoenix <249494353+Flaming-H-Phoenix@users.noreply.github.com>
2026-07-03 18:09:49 +0200

    device management function docstring

--- S10 Machine Learning/ml_fp/Part03/device_management.py
+++ S10 Machine Learning/ml_fp/Part03/device_management.py
@@ -1,6 +1,10 @@
 import torch
 
 def get_device():
+    '''
+    Use torch to find the best device on the hardware to use.
+    Priority order: 'cuda', 'mps, 'cpu'
+    '''
     if torch.cuda.is_available():
         return torch.device("cuda")
     elif torch.backends.mps.is_available():
